4cddd7721912e8d7416de037898ba8d6b68d90e3
Athulyatej <athuvijaya@gmail.com>
2021-09-04 19:40:44 +0530

    Created the project structure

---
+++ src/main/java/com/simple/clone/simpleclonedemo/SimpleCloneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.simple.clone.simpleclonedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCloneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCloneDemoApplication.class, args);
+	}
+
+}
